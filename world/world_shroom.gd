82732a2d0baeead65d0f968765c094e5b6ca5568
divergent-rainbows <divergent.rainbows@gmail.com>
2025-10-23 13:29:11 +0900

    configure player sprite scene

--- world/world_shroom.gd
+++ world/world_shroom.gd
@@ -1,165 +1,158 @@
 extends CharacterBody2D
+class_name Shroomie
 
-enum {North, Northeast, East, Southeast, South, Southwest, West, Northwest}
-enum {Pivot, Walk}
+const IDLE_DELAY 	:= 0.8
+const SNAP_EPSILON 	:= 1.0
+const MIN_SPEED		:= 40.0
+const MAX_SPEED 		:= 70.0
 
-const IDLE_DELAY 	:= 1.0
-const SNAP_EPSILON 	:= 5.0
-const POLL_EPSILON 	:= 10.0
-var movement_speed 	:= 90.0
-var dwell_lock_dir
-var is_moving
-var is_released
-var curr_dir
-var local_pos
-var target_pos
+var _signal_throttle := 0.2
+var _on_aim_cooldown := false
+var _on_move_cooldown := false
+
+var movement_speed 	:= 80.0
+var is_moving 		: bool 
+var is_released 		: bool
+var is_aiming 		: bool
+var local_pos 		: Vector2i
+var target_pos 		: Vector2
+var curr_dir 		: int
 
 @onready var world_map: TileMapLayer = $".."
 @onready var camera: Camera2D = $Camera2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var joystick: TouchScreenJoystick = $"../../HUD/Joystick"
+@onready var selector: HexSelector = $"../HexSelector"
+@onready var hex_joystick: HexJoystick = $"../../HUD/HexJoystick"
 
 func _ready() -> void: 
 	camera.zoom = Vector2(Global.CAMERA_ZOOM, Global.CAMERA_ZOOM)
 	local_pos = Global.current_coords
 	position = world_map.map_to_local(local_pos) 
 	target_pos = position
+	sprite.animation = "idle"
+	is_released = true
 	InputManager.set_platformer_mode(false)
 	_connect_input_signals()
 
-func _connect_input_signals():
-	InputManager.move_left.connect(_on_move_left)
-	InputManager.move_right.connect(_on_move_right)
-	InputManager.move_up.connect(_on_move_up)
-	InputManager.move_down.connect(_on_move_down)
-	InputManager.on_change_direction.connect(_set_h_direction)
-	InputManager.on_zoom.connect(_on_zoom)
-	InputManager.on_release.connect(_on_release)
 
 func _physics_process(_delta: float) -> void:
 	local_pos = world_map.local_to_map(position)
 	Global.current_coords = local_pos
-
-	if position.distance_to(target_pos) < 3.0:
-		position = target_pos
-		if is_released:
-			is_moving = false
-		else:
-			target_pos = world_map.map_to_local(
-				local_pos + get_curr_hex_vector())
-		update_animation()
-	var direction = (target_pos - position).normalized()
-	if dwell_lock_dir == curr_dir:
-		velocity = Vector2.ZERO
-		target_pos = world_map.map_to_local(local_pos)
-	elif is_moving:
-		velocity = direction * movement_speed
+	_snap_location_to_grid()
+	if is_moving:
+		_adjust_velocity()
 		move_and_slide()
-		if get_last_slide_collision() != null:
-			target_pos = position
+
+
+func _connect_input_signals():
+	InputManager.aim.connect(_on_aim)
+	InputManager.move.connect(_on_move)
+	InputManager.on_release.connect(_on_release)
+
+
+func _on_move(dir: int):
+	if _on_move_cooldown: return
+	_on_move_cooldown = true
+	get_tree().create_timer(_signal_throttle).timeout.connect(func(): _on_move_cooldown = false)
+
+	curr_dir = dir
+	is_moving = true
+	is_released = false
+	is_aiming = false
+
+
+func _on_aim(dir: int):
+	if _on_aim_cooldown: return
+	_on_aim_cooldown = true
+	get_tree().create_timer(_signal_throttle).timeout.connect(func(): _on_aim_cooldown = false)
+
+	if not is_moving:
+		curr_dir = dir
+		target_pos = world_map.map_to_local(local_pos + _get_curr_hex_vector())
+		_update_animation()
+		selector.tween_fade_to_target(target_pos)	
+	is_aiming = true
+	is_released = false
+
 
 func _on_release():
 	is_released = true
+	get_tree().create_timer(IDLE_DELAY * 0.5).timeout.connect(_on_release_timeout)
 
-func _on_move_left():
-	curr_dir = West
-	_queue_movement()
 
-func _on_move_right():
-	curr_dir = East
-	_queue_movement()
-func _on_move_up():
-	match curr_dir:
-		Northeast, East, Southeast:
-			curr_dir = Northeast
-		_, Northwest, West, Southwest:
-			curr_dir = Northwest
-	_queue_movement()
-func _on_move_down():
-	match curr_dir:
-		Northeast, East, Southeast:
-			curr_dir = Southeast
-		_, Northwest, West, Southwest:
-			curr_dir = Southwest
-	_queue_movement()
+func _on_release_timeout():
+	if is_released:
+		selector.fade_out()
+		curr_dir = -1
+		get_tree().create_timer(IDLE_DELAY * 0.5).timeout.connect(_on_idle_timeout)
 
-func _queue_movement():
-	is_moving = true
-	is_released = false
 
-func update_animation() -> void:
-	if is_moving:
-		match curr_dir:
-			Northeast:
-				sprite.flip_h = false
-				sprite.play("northeast")
-			Northwest:
-				sprite.flip_h = true
-				sprite.play("northeast")
-			East:
-				sprite.flip_h = false
-				sprite.play("east")
-			West: 
-				sprite.flip_h = true
-				sprite.play("east")
-			Southeast:
-				sprite.flip_h = false
-				sprite.play("southeast")
-			Southwest:
-				sprite.flip_h = true
-				sprite.play("southeast")
+func _on_idle_timeout():
+	if is_released: 
+		sprite.animation = "idle"
+
+
+func _snap_location_to_grid():
+	if position.distance_to(target_pos) < SNAP_EPSILON:
+		position = target_pos
+		if not is_released: 
+			_update_animation()
+		if is_released or is_aiming: 
+			is_moving = false
+		else: _update_target_pos()
+
+
+func _adjust_velocity():
+	var direction = (target_pos - position).normalized()
+	if is_released or is_aiming:
+		movement_speed = max(movement_speed * 0.97, MIN_SPEED)
 	else:
-		sprite.play("idle")
+		movement_speed = MAX_SPEED
+	set_velocity(direction * movement_speed)
 
 
-func get_curr_hex_vector() -> Vector2i:
+func _update_target_pos():
+	target_pos = world_map.map_to_local(
+		local_pos + _get_curr_hex_vector())
+	selector.tween_fade_to_target(target_pos)
+
+
+func _update_animation() -> void:
+	match curr_dir:
+		HexGrid.NE:
+			sprite.flip_h = false
+			sprite.animation = "northeast"
+		HexGrid.NW:
+			sprite.flip_h = true
+			sprite.animation = "northeast"
+		HexGrid.E:
+			sprite.flip_h = false
+			sprite.animation = "east"
+		HexGrid.W: 
+			sprite.flip_h = true
+			sprite.animation = "east"
+		HexGrid.SE:
+			sprite.flip_h = false
+			sprite.animation = "southeast"
+		HexGrid.SW:
+			sprite.flip_h = true
+			sprite.animation = "southeast"
+
+
+func _get_curr_hex_vector() -> Vector2i:
 	var curr_odd = local_pos.y % 2 == 1
 	match curr_dir:
-		Northeast:
+		HexGrid.NE:
 			return Vector2i.UP + (Vector2i.RIGHT if curr_odd else Vector2i.ZERO)
-		East:
+		HexGrid.E:
 			return Vector2i.RIGHT
-		Southeast:
+		HexGrid.SE:
 			return Vector2i.DOWN + (Vector2i.RIGHT if curr_odd else Vector2i.ZERO)
-		Southwest:
+		HexGrid.SW:
 			return Vector2i.DOWN + (Vector2i.ZERO if curr_odd else Vector2i.LEFT)
-		West: 
+		HexGrid.W: 
 			return Vector2i.LEFT
-		Northwest:
+		HexGrid.NW:
 			return Vector2i.UP + (Vector2i.ZERO if curr_odd else Vector2i.LEFT)
 		_:
 			return Vector2i.ZERO
-
-# -1 West, 1 East
-func _set_h_direction(new_dir) -> void:
-	var facing_east = new_dir > 0
-	match curr_dir:
-		Northeast, Northwest:
-			curr_dir = Northeast if facing_east else Northwest
-		East, West:
-			curr_dir = East if facing_east else West
-		Southeast, Southwest:
-			curr_dir = Southeast if facing_east else Southwest
-
-func _calc_h_offset():
-	var curr_odd = local_pos.y % 2 == 1
-	match curr_dir:
-		Northeast, East, Southeast:
-			return Vector2i.RIGHT if curr_odd else Vector2i.ZERO
-		Northwest, West, Southwest:
-			return Vector2i.ZERO if curr_odd else Vector2i.LEFT
-	
-func on_player_entered_tile(_tile: Area2D) -> void:
-	dwell_lock_dir = curr_dir
-
-func on_player_exited_tile(_tile: Area2D) -> void:
-	cancel_dwell()
-
-func cancel_dwell() -> void: 
-	dwell_lock_dir = null
-	
-@onready var camera_2d: Camera2D = $Camera2D
-func _on_zoom(factor) -> void:
-	var minZoom = 2
-	var maxZoom = 8
-	camera.zoom = Vector2(1,1) * clamp(camera.zoom.x * factor, minZoom, maxZoom)
